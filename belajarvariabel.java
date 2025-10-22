16a8d785203273c649d6edc771169d904b58c478
adidtyapr <adidtyapr0701@gmail.com>
2025-10-22 15:41:18 +0700

    xscanner

---
+++ belajarvariabel.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class belajarvariabel{
+    public static void main(String[] args) {
+       int number;
+       Scanner ns = new Scanner(Syestem.in);
+
+       System.out.println(Masukan umur);
+       nUmur = ns.nextInt();
+    }   
+}
